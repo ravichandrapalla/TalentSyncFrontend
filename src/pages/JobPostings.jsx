a7cc34d8ca740f4fd1ac01b9245f5ab3bfaf95c8
ravichandrapalla <cravi6635@gmail.com>
2024-05-21 19:36:20 +0530

    job cards ui revamp two

--- src/pages/JobPostings.jsx
+++ src/pages/JobPostings.jsx
@@ -7,7 +7,9 @@ import toast from "react-hot-toast";
 import { useApplyJobPostingApiHandler } from "../features/authentication/useApplyJobPostingApiHandler";
 import SpinnerComponent from "../ui/SpinnerComponent";
 import MessageComponent from "../ui/MessageComponent";
-import { CiBookmark } from "react-icons/ci";
+import { IoBookmarkOutline } from "react-icons/io5";
+import unKnownCompanyImage from "../assets/images/unKnown.png";
+
 const MainContent = styled.div`
   flex: 1;
   padding: 20px;
@@ -27,9 +29,7 @@ const HeaderStart = styled.div`
 `;
 const JobCount = styled.span`
   font-size: 15px;
-  padding: 5px;
-  border: 1px solid #fed8b1;
-  border-radius: 50px;
+
   color: #888;
 `;
 const HeaderSortSection = styled.div`
@@ -51,7 +51,7 @@ const JobPostingCard = styled.div`
   position: relative;
 `;
 const JobCardPalate = styled.div`
-  background-color: #ffe1cc;
+  background-color: ${(props) => props.bgColor};
   border-radius: 6px;
   display: flex;
   flex-direction: column;
@@ -61,7 +61,7 @@ const JobCardPalate = styled.div`
 const CardSectionOne = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 const JobTitle = styled.h3`
@@ -93,10 +93,11 @@ const JobDate = styled.div`
   color: black;
 `;
 const BookMarkContainer = styled.div`
+  margin-right: 0.2rem;
   background-color: #fff;
-  width: 22px;
-  height: 22px;
-  padding: 5px;
+  width: 25px;
+  height: 25px;
+  padding: 6px;
   border-radius: 50%;
   display: flex;
   align-items: center;
@@ -112,10 +113,22 @@ const JobTitleText = styled.p`
   display: flex;
   align-items: center;
 `;
-const CompanyIcon = styled.div``;
+const CompanyIcon = styled.div`
+  margin-top: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+
+  border-radius: 50%;
+  overflow: hidden;
+`;
 
 const JobTags = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
   justify-content: flex-start;
   align-items: center;
 `;
@@ -134,15 +147,16 @@ const JobLocation = styled.div`
   color: #888;
 `;
 const DeatilsButton = styled.button`
-  padding: 0.5rem;
+  padding: 0.4rem;
   background-color: #007bff;
   color: #fff;
   border: none;
-  border-radius: 50px;
+  margin: 1rem 0.25rem;
+  border-radius: 0.75rem;
   cursor: pointer;
-  position: absolute;
+  /* position: absolute;
   bottom: 20px;
-  right: 20px;
+  right: 20px; */
 `;
 const SortByLabel = styled.label`
   font-family: "orkneyregular", "Outfit Variable", sans-serif;
@@ -174,14 +188,43 @@ const FontTypeSV4 = styled.span`
   font-size: 1.2rem;
   color: #551692;
 `;
+const FontTypeSV5 = styled.span`
+  font-family: "orkneybold", "Outfit Variable", sans-serif;
+  font-size: 1.3rem;
+  color: black;
+`;
+const FontTypeSV6 = styled.label`
+  font-family: "orkneyregular", "Outfit Variable", sans-serif;
+  font-size: 1.3rem;
+  color: #a9a9a9;
+`;
 const JobKeyWords = styled.div`
+  min-width: 6rem;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   border: 1px solid #551692;
   border-radius: 0.7rem;
   background-color: #f1e3ff;
   padding: 0.4rem;
+`;
+const ButtonsContainer = styled.div`
+  width: 45%;
+  /* background-color: red; */
+  display: flex;
+  justify-content: flex-end;
+
+  column-gap: 1rem;
+`;
+const JobCountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 35px;
+  width: 35px;
+  border-radius: 20%;
+  padding: 5px;
+  border: 1px solid #fed8b1;
 `;
 
 const JobPostings = () => {
@@ -221,13 +264,35 @@ const JobPostings = () => {
       },
     });
   };
+  const getColor = (index) => {
+    if (index % 4 === 0) {
+      return "#ffe1cc";
+    } else if (index % 4 === 1) {
+      return "#d4f6ed";
+    } else if (index % 4 === 2) {
+      return "#e3dbfa";
+    } else if (index % 4 === 3) {
+      return "#fbe2f4";
+    }
+  };
+  // const getTagColor = (index) => {};
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    return new Intl.DateTimeFormat("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }).format(date);
+  };
 
   return (
     <MainContent>
       <HeaderSection>
         <HeaderStart>
           <FontLabelHeading>Recommended jobs</FontLabelHeading>
-          <JobCount>386</JobCount>
+          <JobCountContainer>
+            <JobCount>{jobPostings.length}</JobCount>
+          </JobCountContainer>
         </HeaderStart>
         <HeaderSortSection>
           <SortByLabel htmlFor="sort-option">Sort by:</SortByLabel>
@@ -244,15 +309,15 @@ const JobPostings = () => {
         ) : jobPostings.length === 0 ? (
           <p>No job postings found.</p>
         ) : (
-          jobPostings?.map((job) => (
+          jobPostings?.map((job, index) => (
             <JobPostingCard key={job.job_id}>
-              <JobCardPalate>
+              <JobCardPalate bgColor={getColor(index)}>
                 <CardSectionOne>
                   <JobDate>
-                    <FontTypeSV1>20 May, 2023</FontTypeSV1>
+                    <FontTypeSV1>{formatDate(job.created_at)}</FontTypeSV1>
                   </JobDate>
                   <BookMarkContainer>
-                    <CiBookmark />
+                    <IoBookmarkOutline />
                   </BookMarkContainer>
                 </CardSectionOne>
                 <CardSectionOne>
@@ -260,31 +325,48 @@ const JobPostings = () => {
                     <FontTypeSV2>{job.company}</FontTypeSV2>
                     <FontTypeSV3>{job.title}</FontTypeSV3>
                   </CardSectionTwo>
-                  <CompanyIcon>&copy;</CompanyIcon>
+                  <CompanyIcon>
+                    <img
+                      src={`https://logo.clearbit.com/${job.company}.com`}
+                      alt="images"
+                      width={100}
+                      onError={(e) => {
+                        e.target.src = unKnownCompanyImage;
+                      }}
+                    />
+                  </CompanyIcon>
                 </CardSectionOne>
                 <JobTags>
                   <JobKeyWords>
                     <FontTypeSV4>Part time</FontTypeSV4>
                   </JobKeyWords>
-
-                  <span>Senior level</span>
-                  <span>Distant</span>
-                  <span>Project work</span>
+                  <JobKeyWords>
+                    <FontTypeSV4>Senior level</FontTypeSV4>
+                  </JobKeyWords>
+                  <JobKeyWords>
+                    <FontTypeSV4>Project work</FontTypeSV4>
+                  </JobKeyWords>
                 </JobTags>
               </JobCardPalate>
               <JObCardFooter>
                 <CardSectionTwo>
                   <JobSalary>
-                    {parseInt(job.salary) || "Not Disclosed"}
+                    <FontTypeSV5>
+                      {parseInt(job.salary) || "Not Disclosed"}
+                    </FontTypeSV5>
                   </JobSalary>
-                  <JobLocation>{job.currlocation}</JobLocation>
+                  <JobLocation>
+                    <FontTypeSV6>{job.currlocation}</FontTypeSV6>
+                  </JobLocation>
                 </CardSectionTwo>
-                {userData.role === "Client" && (
-                  <DeatilsButton onClick={() => applyJobHandler(job.job_id)}>
-                    Apply
-                  </DeatilsButton>
-                )}
-                <DeatilsButton>Details</DeatilsButton>
+                <ButtonsContainer>
+                  {userData.role === "Client" && (
+                    <DeatilsButton onClick={() => applyJobHandler(job.job_id)}>
+                      Apply
+                    </DeatilsButton>
+                  )}
+                  <DeatilsButton>Details</DeatilsButton>
+                </ButtonsContainer>
               </JObCardFooter>
 
               {/* <JobTitle>{job.title}</JobTitle>
